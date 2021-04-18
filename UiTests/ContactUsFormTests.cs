ba6352fc0d35608f0e8cc2fbd58c9ce2c924650c
Niels Swimberghe <nielsswimberghe@gmail.com>
2021-04-18 01:04:48 -0400

    Initial commit

---
+++ UiTests/ContactUsFormTests.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MailKit;
+using MailKit.Net.Imap;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace Smpt4devSample.UiTests
+{
+    [TestClass]
+    public class ContactUsFormTests
+    {
+        [ClassInitialize]
+        public static async Task SetupTests(TestContext testContext)
+        {
+            var chromeDriverInstaller = new ChromeDriverInstaller();
+            await chromeDriverInstaller.Install();
+        }
+
+        [TestMethod]
+        public void TestContactUsForm()
+        {
+            // output should look like 9014dbca920e40dfa7c760a4a4808759@localhost
+            var emailAddress = $"{Guid.NewGuid().ToString("N")}@localhost";
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:5001");
+                driver.FindElementById("firstName").SendKeys("Jon");
+                driver.FindElementById("lastName").SendKeys("Doe");
+
+                driver.FindElementById("emailAddress").SendKeys(emailAddress);
+
+                driver.FindElementById("question").SendKeys("Hello World!");
+
+                driver.FindElementsByCssSelector("form button").First().Click();
+
+                Assert.AreEqual("https://localhost:5001/Home/ThankYou", driver.Url);
+                Assert.IsTrue(driver.PageSource.Contains("Thank you for contacting us"));
+            }
+
+            using (var client = new ImapClient())
+            {
+                client.Connect("localhost", 143);
+                client.Authenticate(userName: "", password: "");
+
+                // The Inbox folder is always available on all IMAP servers...
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadWrite);
+
+                var emailToSiteOwner = inbox.GetMessage(inbox.Count - 1);
+                var emailToSubmitter = inbox.GetMessage(inbox.Count - 2);
+                MimeKit.MimeMessage tempMessage = null;
+
+                // swap emails if necessary
+                if(
+                    !emailToSiteOwner.Subject.Contains("Contact Us submission by") && 
+                    emailToSubmitter.Subject.Contains("Contact Us submission by")
+                )
+                {
+                    tempMessage = emailToSiteOwner;
+                    emailToSiteOwner = emailToSubmitter;
+                    emailToSubmitter = tempMessage;
+                }
+
+                Assert.AreEqual("Contact Us submission by Jon Doe", emailToSiteOwner.Subject);
+                Assert.IsTrue(emailToSiteOwner.TextBody.Contains("First Name: Jon"));
+                Assert.IsTrue(emailToSiteOwner.TextBody.Contains("Last Name: Doe"));
+                Assert.IsTrue(emailToSiteOwner.TextBody.Contains($"Email Address: {emailAddress}"));
+                Assert.IsTrue(emailToSiteOwner.TextBody.Contains("Hello World!"));
+
+                Assert.AreEqual("Thank you for contacting us", emailToSubmitter.Subject);
+                Assert.IsTrue(emailToSubmitter.TextBody.Contains("Thank you Jon Doe for reaching out to us."));
+
+                client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Smpt4devSample.WebApp.Models;
+
+namespace Smpt4devSample.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult Index(ContactFormViewModel contactFormViewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            using var smtpClient = new SmtpClient()
+            {
+                Host = "localhost",
+                Port = 25
+            };
+            var submitterFullName = $"{contactFormViewModel.FirstName} {contactFormViewModel.LastName}";
+            
+            // send email to website owner
+            smtpClient.Send("website@localhost", "niels@localhost", $"Contact Us submission by {submitterFullName}", 
+$@"New Contact Us submission:
+
+    First Name: {contactFormViewModel.FirstName}
+    Last Name: {contactFormViewModel.LastName}
+    Email Address: {contactFormViewModel.EmailAddress}
+    Question:
+    {contactFormViewModel.Question}"
+            );
+
+            // send email to submitter
+            smtpClient.Send("niels@localhost", contactFormViewModel.EmailAddress, "Thank you for contacting us", 
+                $"Thank you {submitterFullName} for reaching out to us.\nWe have received your inquiry and will respond to you in 24 hours."
+            );
+
+            return RedirectToAction(nameof(ThankYou));
+        }
+
+        public IActionResult ThankYou() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
